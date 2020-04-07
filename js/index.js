76f7a914e039b7d8027ee42153cca08d71927682
toricruzmendiola <mtoricruz@gmail.com>
2020-04-07 13:28:27 -0700

    4/10 eventlisteners added. checkpoint save

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
 // Your code goes here
+
+// Get Function
+const get = (selector) => {
+    return document.querySelector(selector)
+}
+
+// Variables
+const signUpButton = get('.btn')
+const titleWelcome = get('h1')
+const footerZoom = get('footer');
+
+// Double Click
+function welcomeAlert(event){
+    console.log(`on event fired ${event.timeStamp}`)
+    return alert('Welcome to Our Website!')
+}
+
+titleWelcome.addEventListener('dblclick', welcomeAlert)
+
+// Mouse Over
+signUpButton.addEventListener('mouseover', (event) => {
+    event.target.style.color = "red"
+})
+
+// Keydown 
+function escKey(event) {
+    if (event.key === 'Escape') {
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+}
+
+document.addEventListener('keydown', escKey)
+
+// Wheel
+
+function wheelZoom(event) {
+  
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    footerZoom.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  
+  footerZoom.addEventListener('wheel', wheelZoom)
+
+
